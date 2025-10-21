6b6df447aaa403dd7ce138d994deff445932e4a3
Sternvik <Leo,sternvik@gmail.com>
2025-10-21 11:14:12 +0200

    VD Interface, vm koppling

--- PresentationsLager/Views/VD/VDWindow.xaml.cs
+++ PresentationsLager/Views/VD/VDWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntitetsLager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,7 @@ namespace PresentationsLager.Views.VD
     /// </summary>
     public partial class VDWindow : Window
     {
-        public VDWindow()
+        public VDWindow(Anvandare anvandare)
         {
             InitializeComponent();
         }
