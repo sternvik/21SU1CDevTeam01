c712c0fbd240c3c3193b0da5bbdbcc9a83c24748
Sternvik <Leo,sternvik@gmail.com>
2025-10-21 10:34:04 +0200

    VD view

---
+++ PresentationsLager/Views/VD/VDWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PresentationsLager.Views.VD
+{
+    /// <summary>
+    /// Interaction logic for VDWindow.xaml
+    /// </summary>
+    public partial class VDWindow : Window
+    {
+        public VDWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
