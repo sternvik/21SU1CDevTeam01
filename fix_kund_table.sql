06275fac520238f771e405caf29cb2eece75f676
Danzamdanzam <daniel02zam@gmail.com>
2025-10-21 11:27:44 +0200

    Optimering

--- fix_kund_table.sql
+++
@@ -1,8 +0,0 @@
--- Uppdatera Kunder tabellen för att tillåta NULL värden för Email och Telefon
--- Detta löser problemet med att skapa kunder utan email
-
-ALTER TABLE [Kunder] ALTER COLUMN [Email] nvarchar(100) NULL;
-ALTER TABLE [Kunder] ALTER COLUMN [Telefon] nvarchar(20) NULL;
-
--- Kontrollera ändringarna
-SELECT TOP 5 KundID, Namn, Email, Telefon FROM [Kunder];
