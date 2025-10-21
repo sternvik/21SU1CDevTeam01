6e7f0497705871a401c50bf2fd39f4b1ad069f20
VicHackz <victorberg02@hotmail.com>
2025-10-21 13:10:02 +0200

    Code behind fix 2 (VD)

--- PresentationsLager/Views/VD/VDWindow.xaml.cs
+++ PresentationsLager/Views/VD/VDWindow.xaml.cs
@@ -1,28 +1,21 @@
 ﻿using EntitetsLager;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PresentationsLager.ViewModels.VD;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PresentationsLager.Views.VD
 {
-    /// <summary>
-    /// Interaction logic for VDWindow.xaml
-    /// </summary>
     public partial class VDWindow : Window
     {
         public VDWindow(Anvandare anvandare)
         {
             InitializeComponent();
+
+            // Använd samma mönster som ServitorMainWindow
+            if (DataContext is VDWindowViewModel viewModel)
+            {
+                viewModel.Initialize(anvandare);
+                viewModel.CloseAction = () => this.Close();
+            }
         }
     }
 }
