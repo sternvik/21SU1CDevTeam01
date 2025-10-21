ab59575966e192cdb7726663f7c3c183430010c4
Sternvik <Leo,sternvik@gmail.com>
2025-10-21 10:38:30 +0200

    update VD

--- PresentationsLager/ViewModels/VD/VDWindowViewModel.cs
+++ PresentationsLager/ViewModels/VD/VDWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace PresentationsLager.ViewModels.VD
 {
-    internal class VDWindowViewModel
+    public partial class VDWindowViewModel : ObservableObject
     {
     }
 }
