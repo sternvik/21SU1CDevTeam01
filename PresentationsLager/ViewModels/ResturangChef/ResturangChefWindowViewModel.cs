d241de1477d9d4475d0eb107b29c2137a0ffab7a
Sternvik <Leo,sternvik@gmail.com>
2025-10-21 10:37:59 +0200

    update

--- PresentationsLager/ViewModels/ResturangChef/ResturangChefWindowViewModel.cs
+++ PresentationsLager/ViewModels/ResturangChef/ResturangChefWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace PresentationsLager.ViewModels.ResturangChef
 {
-    internal class ResturangChefWindowViewModel
+    public partial class ResturangChefWindowViewModel : ObservableObject
     {
+
     }
 }
